ea3f766b3f4d0c01767e6ddc7af29f72ebcabfa5
Mark C <idelta0@gmail.com>
2024-09-18 11:29:59 +0100

    first attempt at pcap reading

--- cryptomon.py
+++ cryptomon.py
@@ -21,7 +21,9 @@ __status__ = "Demonstration"
 import asyncio
 import argparse
 import psutil
+import sys
 from fapi.config import settings
+from scapy.all import rdpcap, sendp
 
 from cryptomon import CryptoMon
 
@@ -34,6 +36,8 @@ def parse_argz():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--interface", 
                         help="Interface to hook with eBPF module.")
+    parser.add_argument("--pcap", 
+                        help="pcap file")
     args = parser.parse_args()
 
     if not args.interface:
@@ -44,8 +48,26 @@ def parse_argz():
         choice = int(input("Select an interface by number: ")) - 1
         args.interface = interfaces[choice]
 
+    if args.pcap:
+        print("[i] Setting interface to loopback (lo) to parse PCAPs...")
+        args.interface = "lo"
     return args
 
+
+async def rerun_pcap(pcap_file="test.pcap"):
+    packets = rdpcap(pcap_file)
+    iface = "lo"
+    print(f"[i] Replaying packets from {str(pcap_file)}...")
+    ctr = 0
+    for packet in packets:
+        if ctr % 10 == 0:
+            print(f"{ctr} of {len(packets)}", end='\r')
+        try:
+            sendp(packet, iface=iface, verbose=False)
+        except Exception as e:
+            print(f"Error happened when sending packet...: {str(e)}")
+        ctr += 1
+    sys.exit(0)
 
 if __name__ == "__main__":
     task_list = []
@@ -53,9 +75,12 @@ if __name__ == "__main__":
     cm = CryptoMon(iface=args.interface,
                    mongodb=True,
                    settings=settings,
+                   pcap_file=args.pcap,
                    data_tag="")
     loop = asyncio.get_event_loop()
     loop.create_task(cm.run_async())
+    if args.pcap:
+        loop.create_task(rerun_pcap(args.pcap))
     loop.run_forever()
     # alternatively, run...
     # cm.run()
