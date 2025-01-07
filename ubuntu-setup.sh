dd38eac3c27588b7adf3ae8f611f00414a966f08
Mark C. <idelta0@gmail.com>
2025-01-07 14:49:48 +0000

    Update ubuntu-setup.sh for fixing some installation issues.

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl clang llvm jq
 apt-get install -y libelf-dev libpcap-dev libbfd-dev binutils-dev build-essential make 
-apt-get install -y linux-tools-common linux-tools-5.15.0-41-generic bpfcc-tools
+apt-get install -y linux-tools-common linux-tools-5.15.0-41-generic bpfcc-tools python3-dev gcc
 apt-get install -y python3-pip
 apt-get install -y bsdutils
 apt-get install -y build-essential
@@ -12,6 +12,9 @@ apt-get install -y llvm-12 clang-12
 apt-get install -y clang-format-12
 apt-get install -y zlib1g-dev libelf-dev
 apt-get install -y protobuf-compiler
+apt-get install -y python3-scapy
+apt-get install -y python3-motor
+apt-get install -y python3-psutil
 apt-get install bpfcc-tools linux-headers-$(uname -r)
 
 # remove the '-12' suffixes
